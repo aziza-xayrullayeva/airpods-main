6089483fe49560d8d904a771d0abbb08097bbfb4
Xayrullayeva Aziza <xayrullayevaaziza475@gmail.com>
2025-12-09 16:07:43 +0500

    added new banner menu

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+const nav = document.querySelector('.header__nav')
+const btn = document.querySelector('.burger__menu')
+
+btn.addEventListener('click', () => {
+    nav.classList.toggle('active')
+})
